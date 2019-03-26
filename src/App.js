7cb5fffa829fa2629abc00538707c80ada6fee2c
Jamie Uttariello <olinations@gmail.com>
2019-03-25 17:59:37 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,78 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Container, Row, Col } from 'reactstrap'
+import ModalForm from './Components/Modals/Modal'
+import DataTable from './Components/Tables/DataTable'
+import { CSVLink } from "react-csv"
 
 class App extends Component {
+  state = {
+    items: []
+  }
+
+  getItems(){
+    fetch('http://localhost:3000/crud')
+      .then(response => response.json())
+      .then(items => this.setState({items}))
+      .catch(err => console.log(err))
+  }
+
+  addItemToState = (item) => {
+    this.setState(prevState => ({
+      items: [...prevState.items, item]
+    }))
+  }
+
+  updateState = (item) => {
+    const itemIndex = this.state.items.findIndex(data => data.id === item.id)
+    const newArray = [
+    // destructure all items from beginning to the indexed item
+      ...this.state.items.slice(0, itemIndex),
+    // add the updated item to the array
+      item,
+    // add the rest of the items to the array from the index after the replaced item
+      ...this.state.items.slice(itemIndex + 1)
+    ]
+    this.setState({ items: newArray })
+  }
+
+  deleteItemFromState = (id) => {
+    const updatedItems = this.state.items.filter(item => item.id !== id)
+    this.setState({ items: updatedItems })
+  }
+
+  componentDidMount(){
+    this.getItems()
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Container className="App">
+        <Row>
+          <Col>
+            <h1 style={{margin: "20px 0"}}>CRUD Database</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <DataTable items={this.state.items} updateState={this.updateState} deleteItemFromState={this.deleteItemFromState} />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <CSVLink
+              filename={"db.csv"}
+              color="primary"
+              style={{float: "left", marginRight: "10px"}}
+              className="btn btn-primary"
+              data={this.state.items}>
+              Download CSV
+            </CSVLink>
+            <ModalForm buttonLabel="Add Item" addItemToState={this.addItemToState}/>
+          </Col>
+        </Row>
+      </Container>
+    )
   }
 }
 
-export default App;
+export default App
